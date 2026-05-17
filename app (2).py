f46569ab2698c76df52261cdcc6f537e48f02f67
PEDRO HENRIQUE PINHEIRO FALCHI <peeh.falchi@gmail.com>
2026-05-17 16:22:15 -0300

    Implement error handling in model loading and prediction
    Added error handling for model loading and prediction.

--- app (2).py
+++ app (2).py
@@ -5,7 +5,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
-model = joblib.load('modelo_ong_prioridade.joblib')
+try:
+    model = joblib.load('modelo_ong_prioridade.joblib')
+    print("MODELO CARREGADO COM SUCESSO")
+except Exception as e:
+    print("ERRO AO CARREGAR MODELO:")
+    print(e)
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -14,15 +19,21 @@ def health():
 @app.route('/predict', methods=['POST'])
 def predict():
 
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    df = pd.DataFrame([data])
+        df = pd.DataFrame([data])
 
-    prediction = model.predict(df)[0]
+        prediction = model.predict(df)[0]
 
-    return jsonify({
-        'prediction': str(prediction)
-    })
+        return jsonify({
+            'prediction': str(prediction)
+        })
+
+    except Exception as e:
+        return jsonify({
+            'erro': str(e)
+        }), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
